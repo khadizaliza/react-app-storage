3af37547506421255eed94e50f8e975a40f47bc3
khadizaliza <khadizaakter19836@gmail.com>
2022-03-25 12:25:24 +0600

    create-button

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,13 +1,18 @@
 
 import React from 'react';
+import './Cosmetic.css';
 
 const Cosmetic = (props) => {
     const {name, price, id} = props.cosmetic;
+ const addToCart = () =>{
+     console.log('item add', id)
+ }
     return (
-        <div>
+        <div className='product'>
          <h2>Buy this:{name}</h2>
          <p>Only for: ${price}</p>
-         <p><small>It has id: {id}</small></p>   
+         <p><small>It has id: {id}</small></p>
+         <button onClick={() => addToCart(id)}>Add to Cart</button>   
         </div>
     );
 };
